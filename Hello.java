50c406be297045f5b177c8e4926e880cebe0f2eb
ammatthew <54364855+ammatthew@users.noreply.github.com>
2019-08-21 10:17:29 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/*******************************************************************************************************************
+* Hello Java
+********************************************************************************************************************
+* First Lab Assignment
+* Anju Mathew
+* August 21st 2019
+* CMSC 255 Section 3
+********************************************************************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Anju Mathew");
+		System.out.println("Hello Java");
+		System.out.println("First Lab Assignment");
+		System.out.println("August 21st 2019");
+		System.out.println("CMSC 255 Section 3");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
